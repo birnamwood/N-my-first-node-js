72512f6ac9d753583e2fda47dcdefad475b8ed8b
birnamwood <birnamwood19@gmail.com>
2020-02-16 18:31:33 +0900

    3-3 変化率逆ランキングの実装

--- adding-up/app.js
+++ adding-up/app.js
@@ -42,11 +42,11 @@ rl.on('close', () => {
   }
   //普通の配列に変換後Sort
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   })
   //配列のmap関数
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率' + value.change;
+  const rankingStrings = rankingArray.map(([key, value], i) => {
+    return (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率' + value.change;
   });
   console.log(rankingStrings);
 });
